05803e1c00c4fe1952a547ba220bc063154424f0
ravikrushna963 <ravikrushna963@gmail.com>
2022-09-27 19:16:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [statusCode, setStatusCode] = useState("");
+  const [reqBody, setReqBody] = useState("");
+  const [renderResp, setRenderResp] = useState("");
+  const [showData, setShowData] = useState(false);
+
+  useEffect(() => {
+    let requestUrl = "https://gorest.co.in/public-api/users";
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization:
+          "Bearer 88d52bbcb1d08c8abc4749b31118796c1f44c633b20b9ba4c4bfb18e01d1b3f0",
+      },
+      body: reqBody,
+    };
+
+    fetch(requestUrl, options)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonData) {
+        console.log(jsonData.data);
+        setRenderResp(JSON.stringify(jsonData));
+        setStatusCode(jsonData.code);
+      });
+  }, [showData]);
+
+  const onChangeInput = (e) => {
+    setReqBody(e.target.value);
+  };
+
+  const onClickButton = () => {
+    setShowData(!showData);
+  };
+
+  const url = " https://gorest.co.in/public-api/users";
+  // const renderRes = reqBody === "" ? null : reqBody;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="p-3 bg-container">
+      <h1 className="heading mb-4">Post method practice</h1>
+      <p className="request-url-text">
+        REQUEST URL : <span className="request-url">{url}</span>
+      </p>
+      <textarea
+        id="requestBody"
+        placeholder="Enter Request Body"
+        rows="5"
+        cols="50"
+        className="p-2 request-body"
+        onChange={onChangeInput}
+      ></textarea>
+      <button
+        id="sendPostRequestBtn"
+        className="mt-3 p-2 button"
+        onClick={onClickButton}
+      >
+        Send Post Request
+      </button>
+      <hr />
+      <div className="p-2 mt-2">
+        <p className="para1">Request Status</p>
+        <p id="requestStatus" className="request-status">
+          {showData && statusCode}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <hr />
+      <div className="p-2 mt-2">
+        <p className="para1">Response Body</p>
+        <p id="httpResponse" className="http-response">
+          {showData && renderResp}
+        </p>
+      </div>
     </div>
   );
 }
